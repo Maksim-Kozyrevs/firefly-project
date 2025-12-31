05857984d516318bca0f863fea5ed5961676125e
Maksim <kozyrev.maksim.km@gmail.com>
2025-12-31 20:25:55 +0300

    Main

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({path: path.join(__dirname, ".env")});
+
+export const apps = [
+  /*{
+    name: "firefly-server",
+    script: "./apps/node-server/src/server.js",
+    env: process.env
+  },*/
+  {
+    name: "telegram-app",
+    script: "./apps/telegram-app/app.js"
+  }
+];
