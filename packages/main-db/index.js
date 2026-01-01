45cfe7ef71a99538819e7a125eed50461b0119e6
Maksim <kozyrev.maksim.km@gmail.com>
2026-01-01 13:24:32 +0300

    Main

--- packages/main-db/index.js
+++ packages/main-db/index.js
@@ -1,14 +1,9 @@
 import pkg from 'pg';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import envConfig from "@project/env";
 
 
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({
-  path: path.join(__dirname, "../../.env")
-});
+envConfig();
 
 const { Pool } = pkg;
 
