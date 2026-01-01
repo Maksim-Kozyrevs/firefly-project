c37d878cbbe00c9dcd9594cd61f798dfbe0f668d
Maksim <kozyrev.maksim.km@gmail.com>
2026-01-01 13:57:25 +0300

    main

--- test.js
+++ test.js
@@ -1,3 +1,27 @@
-import bcrypt from 'bcrypt';
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+      method: 'POST', // Указываем метод
+      headers: {
+        'Content-Type': 'application/json' // Обязательно для JSON
+      },
+      body: JSON.stringify(data) // Превращаем объект в строку JSON
+    });
 
-console.log(await bcrypt.hash("MaksimScience111!!!", 10));
+    if (!response.ok) {
+      // Если статус не 200-299, выбрасываем ошибку
+      console.error(`Ошибка сервера: ${response.status}`);
+    }
+
+    const result = await response.json(); // Парсим ответ
+    return result;
+}
+
+// Пример использования:
+const apiUrl = "https://api.ai-firefly.ru/yandex/v1.0/token";
+const userData = {
+    grant_type: "authorization_code",
+    code: "5ea193ac-9e3e-4d55-85df-d57e4a694e35"
+};
+
+postData(apiUrl, userData)
+  .then(data => console.log('Ответ от API:', data));
